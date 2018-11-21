826fae36c2b6b90effb412024a0294fa64a8720e
Todd Berta-Oldham <16536474+ToddBertaOldham@users.noreply.github.com>
2018-11-21 00:19:17 -0500

    Rust UEFI text output.
    Loader can now be built and run. Currently only outputs header from C version.

--- Boot/UEFI/Core/src/lib.rs
+++ Boot/UEFI/Core/src/lib.rs
@@ -8,9 +8,10 @@
 
 mod ffi;
 
-pub use self::ffi::{ SystemTable, Handle };
 use self::ffi::*;
 use core::ffi::c_void;
+
+pub use self::ffi::{ SystemTable, Handle, Status };
 
 pub struct UEFIProgram {
     image_handle : Handle,
@@ -23,7 +24,9 @@ impl UEFIProgram {
     }
 
     pub fn disable_watch_timer(&self) {
-        
+        unsafe {
+            ((*(*self.system_table).boot_services).set_watchdog_timer)(0, 0, 0, core::ptr::null_mut::<u16>());
+        }
     }
 
     pub fn exit_boot(&self) {
@@ -32,13 +35,13 @@ impl UEFIProgram {
 
     // Text Output
 
-    fn write_out(&self, protocol : *mut SimpleTextOutputProtocol, string : &str) 
+    fn write_out(&self, protocol : *mut SimpleTextOutputProtocol, string : &str)
     {
-        let length = string.len();
+        let length = string.encode_utf16().count();
 
         if length == 0 
         {
-            return; 
+            return;
         }
 
         unsafe {
@@ -47,16 +50,24 @@ impl UEFIProgram {
             let bs : &BootServices = &*stb.boot_services;
             let op : &SimpleTextOutputProtocol = &*protocol;
 
-            let char_16_buffer : *mut u16 = core::ptr::null_mut();
+            let mut buffer : *mut c_void = core::ptr::null_mut();
             let buffer_size : usize = (length + 1) * core::mem::size_of::<u16>();
 
-            (bs.allocate_pool)(MemoryType::LoaderData, buffer_size, char_16_buffer as *mut *mut c_void);
-        
-            (*char_16_buffer.offset(length as isize)) = 0;
+            (bs.allocate_pool)(MemoryType::LoaderData, buffer_size, &mut buffer as *mut *mut c_void); 
 
-            (op.output_string)(protocol, char_16_buffer);
+            let characters = buffer as *mut u16;
 
-            (bs.free_pool)(char_16_buffer as *mut c_void);
+            let mut next_character = characters;
+            for char16 in string.encode_utf16() {
+                (*next_character) = char16;
+                next_character = next_character.offset(1);
+            }
+
+            (*next_character) = 0;
+
+            (op.output_string)(protocol, buffer as *mut u16);
+
+            (bs.free_pool)(buffer as *mut c_void);
         }
     }
 
--- Boot/UEFI/Loader/src/main.rs
+++ Boot/UEFI/Loader/src/main.rs
@@ -5,17 +5,24 @@
 // *************************************************************************
 
 #![no_std]
+#![no_main]
 
 extern crate uefi_core;
 
 use uefi_core::*;
+use core::panic::PanicInfo;
 
 #[no_mangle]
-pub extern "win64" fn efi_main(image_handle : Handle, system_table : *mut SystemTable) {
+pub extern "win64" fn efi_main(image_handle : Handle, system_table : *mut SystemTable) -> Status {
     let program = UEFIProgram::new(image_handle, system_table);
 
     program.write_console("Pet UEFI Bootloader\r\n");
     program.write_console("Copyright 2018 Todd Berta-Oldham\r\n\r\n");
 
-    while true { }
+    loop { }
+}
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
 }
