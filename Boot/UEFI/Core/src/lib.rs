4d1af0f6b6906165714be2b819e11114c4363688
Todd <16536474+ToddBertaOldham@users.noreply.github.com>
2019-01-29 03:59:10 -0500

    Added global allocator and started replacing UEFISystem for it.

--- Boot/UEFI/Core/src/lib.rs
+++ Boot/UEFI/Core/src/lib.rs
@@ -5,6 +5,7 @@
 // *************************************************************************
 
 #![no_std]
+#![feature(alloc_layout_extra)]
 
 mod ffi;
 mod graphics;
@@ -23,7 +24,89 @@ use core::ffi::c_void;
 use core::ptr::null_mut;
 use core::mem::size_of;
 use core::result::Result;
+use core::alloc::{ GlobalAlloc, Layout };
 
+static mut IMAGE_HANDLE : Option<Handle> = None;
+static mut SYSTEM_TABLE : Option<&SystemTable> = None;
+
+pub fn init(image_handle : Handle, system_table : *mut SystemTable) {
+    unsafe {
+        IMAGE_HANDLE = Some(image_handle);
+        SYSTEM_TABLE = Some(&*system_table);
+    }
+}
+
+// Text Output
+
+pub fn write_to_console(string : &str) {
+    unsafe {
+        let system_table = SYSTEM_TABLE.expect("UEFI system has not been initialized!");
+        write_to_text_protocol(system_table.con_out, string);   
+    }
+}
+
+pub fn write_to_std_error(string : &str) {
+    unsafe {
+        let system_table = SYSTEM_TABLE.expect("UEFI system has not been initialized!");
+        write_to_text_protocol(system_table.con_out, string);   
+    }
+}
+
+unsafe fn write_to_text_protocol(output : *mut SimpleTextOutputProtocol, string : &str) {
+    let length = string.encode_utf16().count();
+
+    if length == 0 {
+        return;
+    }
+
+    let protocol = &*output;
+
+    let layout = Layout::array::<u16>(length + 1).expect("Invalid parameters for layout.");
+    let buffer = ALLOCATOR.alloc(layout);
+    let characters = buffer as *mut u16;
+
+    let mut next_character = characters;
+    for char16 in string.encode_utf16() {
+        (*next_character) = char16;
+        next_character = next_character.offset(1);
+    }
+
+    (*next_character) = 0;
+
+    (protocol.output_string)(output, characters);
+
+    ALLOCATOR.dealloc(buffer, layout); 
+}
+
+// Allocator Definition
+
+struct UEFIAllocator;
+
+unsafe impl GlobalAlloc for UEFIAllocator {
+    unsafe fn alloc(&self, layout: Layout) -> *mut u8 { 
+        let system_table = SYSTEM_TABLE.expect("UEFI system has not been initialized!");
+        let boot_services = &*system_table.boot_services;
+
+        let mut buffer = null_mut::<c_void>();
+        let buffer_size = layout.size();
+
+        ((boot_services.allocate_pool)(MemoryType::LoaderData, buffer_size, &mut buffer as *mut *mut c_void));
+
+        return buffer as *mut u8;
+    }
+
+    unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
+        let system_table = SYSTEM_TABLE.expect("UEFI system has not been initialized!");
+        let boot_services = &*system_table.boot_services;
+
+        (boot_services.free_pool)(ptr as *mut c_void);    
+    }
+}
+
+#[global_allocator]
+static ALLOCATOR : UEFIAllocator = UEFIAllocator;
+
+/* 
 pub struct UEFISystem {
     image_handle : Handle,
     system_table : *mut SystemTable
@@ -128,4 +211,4 @@ impl UEFISystem {
     pub fn memory_map(&self) -> MemoryMap {
         MemoryMap::new(self.system_table)
     }
-}
+} */
--- Boot/UEFI/Loader/src/main.rs
+++ Boot/UEFI/Loader/src/main.rs
@@ -7,34 +7,35 @@
 #![no_std]
 #![no_main]
 
-use uefi_core::*;
+use uefi_core::{Handle, Status, SystemTable};
 use core::panic::PanicInfo;
 
 #[no_mangle]
 pub extern "win64" fn efi_main(image_handle : Handle, system_table : *mut SystemTable) -> Status {
-    let uefi_system = UEFISystem::new(image_handle, system_table);
 
-    prepare_graphics(&uefi_system);
+    uefi_core::init(image_handle, system_table);
+
+    // prepare_graphics(&uefi_system);
     
-    print_header(&uefi_system);
+    print_header();
 
-    let exit_key = prepare_memory_map(&uefi_system);
+    // let exit_key = prepare_memory_map(&uefi_system);
     
    // uefi_system.exit_boot(exit_key);
 
     loop { }
 }
 
-fn print_header(uefi_system : &UEFISystem) {    
-    uefi_system.write_to_console("Pet UEFI Boot Loader\r\n");
-    uefi_system.write_to_console("Copyright 2019 Todd Berta-Oldham\r\n");
+fn print_header() {    
+    uefi_core::write_to_console("Pet UEFI Boot Loader\r\n");
+    uefi_core::write_to_console("Copyright 2019 Todd Berta-Oldham\r\n");
 
     if cfg!(debug_assertions) {
-        uefi_system.write_to_console("This is a debug build.\r\n");
+        uefi_core::write_to_console("This is a debug build.\r\n");
     }
 }
 
-fn prepare_graphics(uefi_system : &UEFISystem) {
+/* fn prepare_graphics(uefi_system : &UEFISystem) {
     let provider = uefi_system.graphics_output_provider();
 
 }
@@ -43,7 +44,7 @@ fn prepare_memory_map(uefi_system : &UEFISystem) -> usize {
     let memory_map = uefi_system.memory_map();
 
     memory_map.key()
-}
+} */
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
