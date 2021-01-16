f30701c2c5660c64a0ab6c153a3d3c68eb72b65a
Aurora <16536474+ToddBertaOldham@users.noreply.github.com>
2021-01-15 21:39:52 -0500

    Returned to mtools.
    I prefer the other method but this should support more than just Linux.

--- scripts/make_image.sh
+++ scripts/make_image.sh
@@ -6,14 +6,21 @@
 # This code is made available under the MIT License.                                               *
 #***************************************************************************************************
 
-rm -r image
-mkdir image
-dd if=/dev/zero of=image/boot.img bs=1M count=100
-mkfs.fat -F 32 image/boot.img
-mmd -i image/boot.img ::/EFI
-mmd -i image/boot.img ::/EFI/BOOT
-mmd -i image/boot.img ::/boot
-mmd -i image/boot.img ::/boot/system
-mcopy -i image/boot.img target/x86_64-unknown-uefi/debug/loader.efi ::EFI/BOOT/BOOTX64.EFI
-mcopy -i image/boot.img target/x86_64-unknown-none/debug/kernel ::boot/system/kernel
-xorriso -as mkisofs -R -f -e boot.img -no-emul-boot -o image/verdure.iso image/
++# Remove old directory and create a new one.
+rm -f -r image
+mkdir -p image/src/
+
+# Create FAT16 boot image for UEFI.
+dd if=/dev/zero of=image/src/boot.img bs=512 count=32000
+mkfs.fat -F 16 image/src/boot.img
+
+# Copy files to boot image.
+mmd -i image/src/boot.img ::/EFI
+mmd -i image/src/boot.img ::/EFI/BOOT
+mmd -i image/src/boot.img ::/boot
+mmd -i image/src/boot.img ::/boot/system
+mcopy -i image/src/boot.img target/x86_64-unknown-uefi/debug/loader.efi ::EFI/BOOT/BOOTX64.EFI
+mcopy -i image/src/boot.img target/x86_64-unknown-none/debug/kernel ::boot/system/kernel
+
+# Create ISO.
+xorriso -as mkisofs -R -f -e boot.img -no-emul-boot -c boot.cat -V "VERDURE_OS" -A "Verdure OS" -iso-level 3 -o image/verdure_os.iso image/src
