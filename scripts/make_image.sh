d7f150d5a97820cb9d53b5b5dde8bffe3a35273c
Aurora <16536474+ToddBertaOldham@users.noreply.github.com>
2021-01-15 21:18:25 -0500

    Revert "Changed make_image.sh process."
    This reverts commit 30837f94bc5f018566ce6fc4f817bf65d01cd105.

--- scripts/make_image.sh
+++ scripts/make_image.sh
@@ -6,26 +6,14 @@
 # This code is made available under the MIT License.                                               *
 #***************************************************************************************************
 
-# Remove old directory and create a new one.
-rm -f -r image
-mkdir -p image/src/boot
-
-# Create FAT16 boot image for UEFI.
-dd if=/dev/zero of=image/src/boot.img bs=512 count=32000
-mkfs.fat -F 16 image/src/boot.img
-
-# Mount boot image.
-mount -t msdos image/src/boot.img image/src/boot -o loop
-
-# Copy files to boot image.
-mkdir -p image/src/boot/EFI/BOOT/
-mkdir -p image/src/boot/boot/system/
-cp target/x86_64-unknown-uefi/debug/loader.efi image/src/boot/EFI/BOOT/BOOTX64.EFI
-cp  target/x86_64-unknown-none/debug/kernel image/src/boot/boot/system/kernel
-
-# Unmount boot image.
-umount image/src/boot
-rm -r image/src/boot
-
-# Create ISO.
-xorriso -as mkisofs -R -f -e boot.img -no-emul-boot -c boot.cat -V "VERDURE_OS" -A "Verdure OS" -iso-level 3 -o image/verdure_os.iso image/src
+rm -r image
+mkdir image
+dd if=/dev/zero of=image/boot.img bs=1M count=100
+mkfs.fat -F 32 image/boot.img
+mmd -i image/boot.img ::/EFI
+mmd -i image/boot.img ::/EFI/BOOT
+mmd -i image/boot.img ::/boot
+mmd -i image/boot.img ::/boot/system
+mcopy -i image/boot.img target/x86_64-unknown-uefi/debug/loader.efi ::EFI/BOOT/BOOTX64.EFI
+mcopy -i image/boot.img target/x86_64-unknown-none/debug/kernel ::boot/system/kernel
+xorriso -as mkisofs -R -f -e boot.img -no-emul-boot -o image/verdure.iso image/
